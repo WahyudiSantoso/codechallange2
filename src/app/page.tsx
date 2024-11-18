9701a147a819e5b8e6536a1bd0b6c222c739f773
WahyudiSantoso <wahyudisantoso9721@gmail.com>
2024-11-18 18:11:12 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,99 +2,174 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <nav className="navbar">
+        <a href="#" className="navbar-logo">
+          kevala<span>ceramics</span>.
+        </a>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+        <div className="navbar-nav">
+          <a href="#home">Home</a>
+          <a href="#about">Tentang kami</a>
+          <a href="#products">Produk</a>
+          <a href="#contact">Blog</a>
+          <a href="#blog">Kontak</a>
+        </div>
+
+        <div className="navbar-extra">
+          <a href="#" id="search">
+            <i data-feather="search"></i>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a href="#" id="shopping-cart">
+            <i data-feather="shopping-cart"></i>
+          </a>
+          <a href="#" id="menu">
+            <i data-feather="menu"></i>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </nav>
+
+      <section className="hero" id="home">
+        <main className="content">
+          <h1>
+            Lorem, ipsum dolor. <span>ceramics</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, sed.
+          </p>
+          <a href="#" className="cta">
+            Beli sekarang
+          </a>
+        </main>
+      </section>
+
+      <section id="about" className="about">
+        <h2>
+          <span>Tentang</span> kami
+        </h2>
+
+        <div className="row">
+          <div className="about-img">
+            <img src="img/about-up.jpg" alt="About Up" />
+          </div>
+          <div className="content">
+            <h3>Kenapa memilih ceramics kami?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni,
+              sint? Aliquid nobis facilis, quo vel quas alias adipisci officia
+              iusto!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+              totam placeat dolorem, sint aperiam sunt repudiandae non laborum
+              vel quod.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section id="products" className="products">
+        <h2>
+          <span>Produk</span> kami
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          dolorum quo voluptas off icia velit. Voluptatum.
+        </p>
+        <div className="row">
+          <div className="products-card">
+            <img src="img/1.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- bowl -</h3>
+            <p className="products-card-price">IDR 100K</p>
+          </div>
+          <div className="products-card">
+            <img src="img/2.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- allo -</h3>
+            <p className="products-card-price">IDR 200K</p>
+          </div>
+          <div className="products-card">
+            <img src="img/1.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- bowl -</h3>
+            <p className="products-card-price">IDR 100K</p>
+          </div>
+          <div className="products-card">
+            <img src="img/1.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- bowl -</h3>
+            <p className="products-card-price">IDR 100K</p>
+          </div>
+          <div className="products-card">
+            <img src="img/1.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- bowl -</h3>
+            <p className="products-card-price">IDR 100K</p>
+          </div>
+          <div className="products-card">
+            <img src="img/1.jpg" alt="bowl" className="products-card-img" />
+            <h3 className="products-card-title">- bowl -</h3>
+            <p className="products-card-price">IDR 100K</p>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact" className="contact">
+        <h2>
+          <span>Kontak</span> kami
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, facere!
+        </p>
+
+        <div className="row">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114608.267027784!2d112.630281347511!3d-7.275441716727254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbf8381ac47f%3A0x3027a76e352be40!2sSurabaya%2C%20Jawa%20Timur!5e1!3m2!1sid!2sid!4v1731517469219!5m2!1sid!2sid"
+            loading="lazy"
+            className="map"
+          ></iframe>
+
+          <form action="">
+            <div className="input-group">
+              <i data-feather="user"></i>
+              <input type="text" placeholder="name" />
+            </div>
+            <div className="input-group">
+              <i data-feather="mail"></i>
+              <input type="text" placeholder="email" />
+            </div>
+            <div className="input-group">
+              <i data-feather="phone"></i>
+              <input type="text" placeholder="handphone" />
+            </div>
+            <button type="submit" className="btn">
+              kirim pesan
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <footer>
+        <div className="socials">
+          <a href="#">
+            <i data-feather="instagram"></i>
+          </a>
+          <a href="#">
+            <i data-feather="twitter"></i>
+          </a>
+          <a href="#">
+            <i data-feather="facebook"></i>
+          </a>
+        </div>
+
+        <div>
+          <a href="#home">Home</a>
+          <a href="#about">Tentang kami</a>
+          <a href="#products">Produk</a>
+          <a href="#contact">Kontak</a>
+        </div>
+
+        <div className="credit">
+          <p>
+            Created by <a href="">wahyudisantoso</a>. | &copy; 2024.
+          </p>
+        </div>
       </footer>
     </div>
   );
